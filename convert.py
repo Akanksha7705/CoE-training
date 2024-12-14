44bd1e3045c206b7e8b7c94cf28ad3482319bd9a
Akanksha7705 <akankshakatherapaka@gmail.com>
2024-12-14 19:59:02 +0530

    commit

---
+++ convert.py
@@ -0,0 +1,8 @@
+feet = int(input("Enter distance in feet: "))
+inch = 12 * feet
+yard = feet/3
+mile = feet/5280
+print("Distance in feet: ", feet)
+print("Distance in inch: ", inch)
+print("Distance in yard: ", yard)
+print("Distance in mile: ", mile)
