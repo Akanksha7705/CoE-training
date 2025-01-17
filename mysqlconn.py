4023e02fe41ed468f9d94fd53b5b60d889f5e27d
Akanksha K <akankshakatherapaka@gmail.com>
2025-01-17 14:00:41 +0530

    DML commands

--- mysqlconn.py
+++ mysqlconn.py
@@ -9,14 +9,22 @@ mycursor = mydb.cursor()
 mycursor.execute("create table emp(empid int, ename varchar(20), city varchar(20), sal int)")
 
 # manual inputs to insert data into tables
-# name = input("Enter name: ")
-# id = input("Enter id: ")
-# city = input("Enter city: ")
-# sal = input("Enter salary: ")
-# mycursor.execute("insert into emp values(%s,%s,%s,%s)", (id, name,city,sal))
+name = input("Enter name: ")
+id = input("Enter id: ")
+city = input("Enter city: ")
+sal = input("Enter salary: ")
+mycursor.execute("insert into emp values(%s,%s,%s,%s)", (id, name,city,sal))
 
-mycursor.execute("delete from emp where empid = 101") #delete
-mycursor.execute("UPDATE emp SET ename = 'Mary', city = 'San Francisco', sal = 55000 WHERE empid = 1") #update details
+# delete employee details manually by id 
+id=input("Enter id to be deleted: ")
+mycursor.execute("delete from emp where empid = %s",(id,)) #delete
+
+#update details of employees manually
+name = input("Enter name updated: ")
+id = input("Enter id updated: ")
+city = input("Enter city updated: ")
+sal = input("Enter salary updated: ")
+mycursor.execute("UPDATE emp SET ename = %s, city =  %s, sal =%s WHERE empid = %s", (name,city,sal,id)) #update details
 
 #display all details
 print("\nDisplaying all the details")
@@ -32,12 +40,14 @@ employees = mycursor.fetchall()
 for employee in employees:
     print(employee)
 
+# print Details of employees earning greater than 50k and less than 70k
 print("\nDetails of employees earning greater than 50k and less than 70k")
 mycursor.execute("select * from emp where sal > 50000 and sal < 70000")
 employees = mycursor.fetchall()
 for employee in employees:
     print(employee)
 
+# display details of employees whose city is Los Angeles
 print("\nDetails of employees whose city is Los Angeles")
 mycursor.execute("select * from emp where city='Los Angeles'")
 employees = mycursor.fetchall()
